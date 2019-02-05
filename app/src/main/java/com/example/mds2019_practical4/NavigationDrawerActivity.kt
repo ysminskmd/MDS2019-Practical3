7f881fd8b4f486e80b29283e49b7da0c84a26937
Stepan Churyukanov <tewnn@yandex-team.ru>
2019-02-05 23:19:44 +0300

    Add scrolling activity

--- app/src/main/java/com/example/mds2019_practical4/NavigationDrawerActivity.kt
+++ app/src/main/java/com/example/mds2019_practical4/NavigationDrawerActivity.kt
@@ -1,13 +1,15 @@
 package com.example.mds2019_practical4
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_navigation_drawer.*
 import kotlinx.android.synthetic.main.app_bar_navigation_drawer.*
 
@@ -30,6 +32,12 @@ class NavigationDrawerActivity : AppCompatActivity(), NavigationView.OnNavigatio
         toggle.syncState()
 
         navView.setNavigationItemSelectedListener(this)
+
+        val navigationHeaderView = navView.getHeaderView(0)
+        val profileImage = navigationHeaderView.findViewById<ImageView>(R.id.imageView)
+        profileImage.setOnClickListener {
+            startActivity(Intent(this@NavigationDrawerActivity, ScrollingActivity::class.java))
+        }
     }
 
     override fun onBackPressed() {
