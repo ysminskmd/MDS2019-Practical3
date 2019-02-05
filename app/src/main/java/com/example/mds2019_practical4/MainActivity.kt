465af10311396621824b0e6f6d47544c56b03395
Stepan Churyukanov <tewnn@yandex-team.ru>
2019-02-05 22:36:57 +0300

    Snackbar

--- app/src/main/java/com/example/mds2019_practical4/MainActivity.kt
+++ app/src/main/java/com/example/mds2019_practical4/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.mds2019_practical4
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BaseTransientBottomBar
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "Ya:Snackbar"
 
 class MainActivity : AppCompatActivity() {
+
+    private val catSnackbar = lazy(LazyThreadSafetyMode.NONE) {
+        Snackbar
+            .make(parentView, "Не забудь покормить кота", Snackbar.LENGTH_LONG)
+            .setAction("Перебьётся") {}
+            .addCallback(object : BaseTransientBottomBar.BaseCallback<Snackbar>() {
+                override fun onShown(transientBottomBar: Snackbar?) {
+                    Log.i(TAG, "Snackbar shown")
+                }
+
+                override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                    val logMessage = when (event) {
+                        Snackbar.Callback.DISMISS_EVENT_ACTION -> "Закрыт кнопкой"
+                        Snackbar.Callback.DISMISS_EVENT_TIMEOUT -> "Закрыт по таймауту"
+                        else -> "Закрыт по прочей причине"
+                    }
+                    Log.i(TAG, logMessage)
+                }
+            })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonPushMe.setOnClickListener(this::showSnack)
+    }
+
+    private fun showSnack(view: View) {
+        catSnackbar.value.show()
     }
 }
